464d83a558939c35c6c2df3e50a257e18eb532b7
Shahir Jalal <shahirjalalhazli@gmail.com>
2024-05-27 20:36:20 +0800

    added new API

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,41 +1,43 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { User } from '../model/user.model';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {User} from '../model/user.model';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class UserService {
-    private apiUrl = 'http://localhost:8080/api/users';
+  // private apiUrl = 'http://localhost:8080/api/users';
+  private apiUrl = 'https://taskit-backend-551e091e845d.herokuapp.com/api/users';
 
-    constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
-    // Get all users
-    getUsers(): Observable<User[]> {
-        return this.http.get<User[]>(this.apiUrl);
-    }
+  // Get all users
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.apiUrl);
+  }
 
-    // Get a single user by ID
-    getUserById(id: number): Observable<User> {
-        const url = `${this.apiUrl}/${id}`;
-        return this.http.get<User>(url);
-    }
+  // Get a single user by ID
+  getUserById(id: number): Observable<User> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.get<User>(url);
+  }
 
-    // Add a new user
-    addUser(user: User): Observable<User> {
-        return this.http.post<User>(this.apiUrl, user);
-    }
+  // Add a new user
+  addUser(user: User): Observable<User> {
+    return this.http.post<User>(this.apiUrl, user);
+  }
 
-    // Update an existing user
-    updateUser(user: User): Observable<User> {
-        const url = `${this.apiUrl}/${user.id}`;
-        return this.http.put<User>(url, user);
-    }
+  // Update an existing user
+  updateUser(user: User): Observable<User> {
+    const url = `${this.apiUrl}/${user.id}`;
+    return this.http.put<User>(url, user);
+  }
 
-    // Delete a user by ID
-    deleteUser(id: number): Observable<any> {
-        const url = `${this.apiUrl}/${id}`;
-        return this.http.delete(url);
-    }
+  // Delete a user by ID
+  deleteUser(id: number): Observable<any> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.delete(url);
+  }
 }
